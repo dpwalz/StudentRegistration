bd6b0ea269cfad2a8724405f521d416d95477602
Derek Walz <derek.walz@ucalgary.ca>
2022-11-18 11:09:48 -0700

    updated student script

--- frontend/new/studentlist.js
+++ frontend/new/studentlist.js
@@ -1,4 +1,4 @@
-let data = ["ENSF 592", "ENSF 593", "ENSF 594", "ENSF 607"];
+let data = ["ENSF 592 01", "ENSF 593 02", "ENSF 594 03", "ENSF 607 01"];
   
 let list = document.getElementById("myList");
 let dropList = document.getElementById("dropList");
@@ -13,7 +13,6 @@ data.forEach((item) => {
         dropItem.innerText = item;
         dropList.appendChild(dropItem);
         list.removeChild(li);
-        dropForm.nodeValue += item + "\n";
         
     };
     li.innerText = item;
@@ -22,14 +21,21 @@ data.forEach((item) => {
 });
 
 function dropCourses(){
-
-
-
-
+    
     let courses = document.getElementById("dropList").querySelectorAll("li");
     let courseList = '';
+    let courseObj = [];
     courses.forEach((item) => {
+        let courseParse = item.innerText.split(" ");
+        let courseDict = {
+            'CourseName': courseParse[0],
+            'CourseID': courseParse[1],
+            'Section': courseParse[2]
+        };
+        courseObj.push(courseDict);
         courseList += item.innerText + '\n';
     });
+    console.log(courseObj);
+    console.log(courseList);
     alert('Dropping:\n' + courseList);
 }
