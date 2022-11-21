ac4d003c2c3c982852eb5750967b4579deb68432
Derek Walz <derek.walz@ucalgary.ca>
2022-11-21 09:42:32 -0700

    functional teacher view

--- App/src/main/resources/static/teacherlist.js
+++ App/src/main/resources/static/teacherlist.js
@@ -1,8 +1,8 @@
 let data = ["Derek Walz 11234", "Brock Lesner 123456789", "Tina Turner 12345799"];
-let course = ["ENSF 592 sec 1", "ENSF 593 sec 2"];
+let course = ["ENSF 592 1", "ENSF 593 2"];
 let coursedict = {
-    "ENSF 592 sec 1": ["Derek Walz 11234", "Brock Lesner 123456789", "Tina Turner 12345799"],
-    "ENSF 593 sec 2": ["Bob Bigelow 11234", "Brock Lesner 123456789", "Tina Turner 12345799"]
+    "ENSF 592 1": ["Derek.Walz 11234 A", "Brock.Lesner 123456789 ", "Tina.Turner 12345799 F"],
+    "ENSF 593 2": ["Bob.Bigelow 11234 ", "Brock.Lesner 123456789 C", "Tina.Turner 12345799"]
 }
 
 // add a div for the course and sections with radio button, 
@@ -44,9 +44,10 @@ function getStudents(item){
     let students = coursedict[item];
     list.innerHTML = "";
     students.forEach((item) => {
+        let details = item.split(" ");
         let li = document.createElement("li");
-        let grade = makeGrades()
-        li.innerText = item;
+        let grade = makeGrades(details[2]);
+        li.innerText = details[0] + " " + details[1];
         list.appendChild(li);
         li.appendChild(grade);
     });
@@ -55,8 +56,17 @@ function getStudents(item){
 function addGrades(){
     
     let students = document.getElementById("stuList").querySelectorAll("li");
+    let courses = document.querySelectorAll("input[name=courses]");
     let studentList = '';
     let studentObj = [];
+    let current_course = [];
+    let re = /.*/;
+
+    courses.forEach((course) => {
+        if(course.checked){
+            current_course = course.value.split(" "); 
+        }
+    });
 
     students.forEach((item) => {
         
@@ -64,8 +74,11 @@ function addGrades(){
         let grade = grades.options[grades.options.selectedIndex].value;
         let courseParse = item.innerText.split(" ");
         let courseDict = {
+            'CourseName': current_course[0],
+            'CourseID': current_course[1],
+            'CourseSection': current_course[2],
             'StudentName': courseParse[0],
-            'StudentID': courseParse[1],
+            'StudentID': courseParse[1].match(re)[0],
             'Grade': grade
         };
         studentObj.push(courseDict);
@@ -75,14 +88,17 @@ function addGrades(){
 }
 
 
-function makeGrades() {
+function makeGrades(grade) {
     let options = document.createElement("select");
     options.name = 'studentsgrade';
-    let myOptions = ['A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D+', 'D', 'D-', 'F'];
+    let myOptions = [' ', 'A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D+', 'D', 'D-', 'F'];
     myOptions.forEach((item) => {
         let new_option = document.createElement("option");
         new_option.value = item;
         new_option.innerText = item;
+        if(item === grade){
+            new_option.selected = true;
+        }
         options.appendChild(new_option);
     });
     return options;
